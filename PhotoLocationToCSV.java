904e8129e885977e249790dc25ea39c7c50a01b7
ibrhmyzc <ibrhmyazici@gmail.com>
2020-08-12 18:45:38 +0300

    final

---
+++ PhotoLocationToCSV.java
@@ -0,0 +1,40 @@
+package photolocation;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class PhotoLocationToCSV {
+    private static BufferedWriter bw = null;
+    private static final String INPUT_DIRECTORY = "D:\\LocationTestFolder";
+    private static final String OUTPUT_FILENAME = "locations.csv";
+
+    public static void main(String[] args) throws IOException {
+        bw = new BufferedWriter(new FileWriter(new File(OUTPUT_FILENAME)));
+        Files.find(Paths.get(INPUT_DIRECTORY),
+                Integer.MAX_VALUE,
+                (filePath, fileAttr) -> fileAttr.isRegularFile())
+                .parallel()
+                .forEach(f -> {
+                    System.out.println(f);
+                    try {
+                        javaxt.io.Image image = new javaxt.io.Image(f.toAbsolutePath().toString());
+                        double[] gps = image.getGPSCoordinate();
+                        if(gps != null) {
+                            bw.write("Location,"
+                                    + gps[1] + "° " + (image.getGpsTags().get(1).equals("N") ? "N" : "S") + ","
+                                    + gps[0] + "° " + (image.getGpsTags().get(3).equals("E") ? "E" : "W") + "\n"
+                            );
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+        bw.close();
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Geotag-to-CSV
+
+Extracts geo tag infos  of photos from a directory(being recursively scanned) and writes them to a csv file which can be imported to google maps
